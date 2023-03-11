d7175914759c8d90e5cced1d91f9c513813e5245
RWTHMyScore <121870055+RWTHMyScore@users.noreply.github.com>
2023-03-11 11:05:17 +0100

    Delete startanddeploy.sh

--- startanddeploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -m
-ganache -i 1337 --chain.chainId 1337 -h 0.0.0.0 -p 8546 -b 5 -e 2 --wallet.mnemonic "$MNEMONIC" &
-sleep 2
-truffle migrate --network dev
-./copyABIs.sh
-fg %1
-trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
