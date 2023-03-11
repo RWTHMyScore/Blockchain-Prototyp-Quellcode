023a042c6d2fc80902f9726f69df4aecafc142a4
RWTHMyScore <121870055+RWTHMyScore@users.noreply.github.com>
2023-03-11 11:04:17 +0100

    Delete finaldeploy.sh

--- finaldeploy.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-PARTNER_A_UNIVERSITY="RWTH" PARTNER_A_DEPARTMENT="Department of Computer Science" PARTNER_A_SERVER="https://localhost:3000" PARTNER_B_PUBKEY="0x7c24D17e8D81C0688CE1E858b151989Be54C45F9" PARTNER_B_UNIVERSITY="NUS" PARTNER_B_DEPARTMENT="School of Computing" PARTNER_B_SERVER="https://localhost:3001" truffle migrate --network goerli
